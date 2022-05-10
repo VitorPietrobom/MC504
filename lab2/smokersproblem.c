b46c2dd6270c026628ceebec7466b2a9346e7e1c
Guilherme <guilherme.tezoli@hotmail.com>
2022-05-09 23:24:41 -0300

    ajustes finais - finalização do programa

--- lab2/smokersproblem.c
+++ lab2/smokersproblem.c
@@ -5,17 +5,18 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-const int ID_AGENTE_PAPEL_TABACO = 1;
-const int ID_AGENTE_TABACO_FOSFORO = 2;
-const int ID_AGENTE_FOSFORO_PAPEL = 3;
+const int ID_AGENTE_PAPEL_TABACO = 0;
+const int ID_AGENTE_TABACO_FOSFORO = 1;
+const int ID_AGENTE_FOSFORO_PAPEL = 2;
 
-const int ID_FUMANTE_PAPEL = 1;
-const int ID_FUMANTE_TABACO = 2;
-const int ID_FUMANTE_FOSFORO = 3;
+const int ID_FUMANTE_PAPEL = 0;
+const int ID_FUMANTE_TABACO = 1;
+const int ID_FUMANTE_FOSFORO = 2;
 
 volatile bool finishProgram = false;
 
 volatile bool temTabaco, temFosforo, temPapel = false;
+
 sem_t semaforo_agentes;
 sem_t semaforo_agente_papel_tabaco;
 sem_t semaforo_agente_tabaco_fosforo;
@@ -34,21 +35,21 @@ pthread_mutex_t lock;
 
 void *thread_semaforo_agente() {
     // chama os semaforos de agentes randomicamente
-    printf("INicio!");
-    int id_agente;
-    for (int i = 0; i < 3; i++) {
+    int id_agente = 0;
+    for (int i = 0; i < 10; i++) {
         sem_wait(&semaforo_agentes);
+        //srand ( time(NULL) );
         id_agente = rand() % 3;
         if (id_agente == ID_AGENTE_PAPEL_TABACO) {
-            sem_post(&semaforo_papel);
+            sem_post(&semaforo_agente_papel_tabaco);
         } else if (id_agente == ID_AGENTE_TABACO_FOSFORO) {
-            sem_post(&semaforo_tabaco);
+            sem_post(&semaforo_agente_tabaco_fosforo);
         } else if (id_agente == ID_AGENTE_FOSFORO_PAPEL) {
-            sem_post(&semaforo_fosforo);
+            sem_post(&semaforo_agente_fosforo_papel);
         }
     }
     finishProgram = true;
-    return 0;
+    return NULL;
 }
 
 
@@ -58,7 +59,7 @@ void *thread_agente_papel_tabaco() {
         sem_post(&semaforo_papel);
         sem_post(&semaforo_tabaco);
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_agente_tabaco_fosforo() {
@@ -67,7 +68,7 @@ void *thread_agente_tabaco_fosforo() {
         sem_post(&semaforo_tabaco);
         sem_post(&semaforo_fosforo);
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_agente_fosforo_papel() {
@@ -76,7 +77,7 @@ void *thread_agente_fosforo_papel() {
         sem_post(&semaforo_papel);
         sem_post(&semaforo_fosforo);
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_pusher_papel() {
@@ -97,7 +98,7 @@ void *thread_pusher_papel() {
         // destravar mutex
         pthread_mutex_unlock(&lock);
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_pusher_tabaco() {
@@ -118,7 +119,7 @@ void *thread_pusher_tabaco() {
         // destravar mutex
         pthread_mutex_unlock(&lock);
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_pusher_fosforo() {
@@ -140,11 +141,11 @@ void *thread_pusher_fosforo() {
         pthread_mutex_unlock(&lock);
 
     }
-    return 0;
+    return NULL;
 }
 
 void fumar(char* nome_fumante) {
-    printf("Fumante do %s fumou!", nome_fumante);
+    printf("Fumante do %s fumou!\n", nome_fumante);
 }
 
 void fazer_cigarro(int id_fumante) {
@@ -163,31 +164,34 @@ void fazer_cigarro(int id_fumante) {
 void *thread_fumante_papel() {
     while(!finishProgram) {
         sem_wait(&semaforo_fumante_papel);
+        if(finishProgram) break;
         fazer_cigarro(ID_FUMANTE_PAPEL);
         sem_post(&semaforo_agentes);
         fumar("Papel");
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_fumante_tabaco() {
     while(!finishProgram) {
         sem_wait(&semaforo_fumante_tabaco);
+        if(finishProgram) break;
         fazer_cigarro(ID_FUMANTE_TABACO);
         sem_post(&semaforo_agentes);
         fumar("Tabaco");
     }
-    return 0;
+    return NULL;
 }
 
 void *thread_fumante_fosforo() {
     while(!finishProgram) {
         sem_wait(&semaforo_fumante_fosforo);
+        if(finishProgram) break;
         fazer_cigarro(ID_FUMANTE_FOSFORO);
         sem_post(&semaforo_agentes);
         fumar("Fosforo");
     }
-    return 0;
+    return NULL;
 }
 
 int main() {
@@ -195,6 +199,10 @@ int main() {
     pthread_t agente_papel_tabaco;
     pthread_t agente_tabaco_fosforo;
     pthread_t agente_fosforo_papel;
+    
+    pthread_t pusher_papel;
+    pthread_t pusher_tabaco;
+    pthread_t pusher_fosforo;
 
     pthread_t fumante_papel;
     pthread_t fumante_tabaco;
@@ -224,19 +232,47 @@ int main() {
     pthread_create(&agente_tabaco_fosforo, NULL, thread_agente_tabaco_fosforo, NULL);
     pthread_create(&agente_fosforo_papel, NULL, thread_agente_fosforo_papel, NULL);
 
+    pthread_create(&pusher_papel, NULL, thread_pusher_papel, NULL);
+    pthread_create(&pusher_tabaco, NULL, thread_pusher_tabaco, NULL);
+    pthread_create(&pusher_fosforo, NULL, thread_pusher_fosforo, NULL);
+
+
     pthread_create(&fumante_papel, NULL, thread_fumante_papel, NULL);
     pthread_create(&fumante_tabaco, NULL, thread_fumante_tabaco, NULL);
     pthread_create(&fumante_fosforo, NULL, thread_fumante_fosforo, NULL);
-    // para as threads
+    // // para as threads
 
-    pthread_join(agentes, 0);
-    pthread_join(agente_papel_tabaco, 0);
-    pthread_join(agente_tabaco_fosforo, 0);
-    pthread_join(agente_fosforo_papel, 0);
+    pthread_join(agentes, NULL);
 
-    pthread_join(fumante_papel, 0);
-    pthread_join(fumante_tabaco, 0);
-    pthread_join(fumante_fosforo, 0);
+    sem_post(&semaforo_agentes);
+    sem_post(&semaforo_agente_papel_tabaco);
+    sem_post(&semaforo_agente_tabaco_fosforo);
+    sem_post(&semaforo_agente_fosforo_papel);
 
-    return 0;
+    sem_post(&semaforo_papel);
+    sem_post(&semaforo_tabaco);
+    sem_post(&semaforo_fosforo);
+
+    sem_post(&semaforo_fumante_papel);
+    sem_post(&semaforo_fumante_tabaco);
+    sem_post(&semaforo_fumante_fosforo);
+
+    pthread_join(agente_papel_tabaco, NULL);
+    pthread_join(agente_tabaco_fosforo, NULL);
+    pthread_join(agente_fosforo_papel, NULL);
+
+    pthread_join(pusher_papel, NULL);
+    pthread_join(pusher_tabaco, NULL);
+    pthread_join(pusher_fosforo, NULL);
+
+
+
+    pthread_join(fumante_papel, NULL);
+    pthread_join(fumante_tabaco, NULL);
+    pthread_join(fumante_fosforo, NULL);
+
+
+    pthread_mutex_destroy(&lock);
+
+     return 0;
 }
